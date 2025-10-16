1245bce8d97ed60bb68b3cd9996379f3b28d44b5
dasl2001 <70586727+dasl2001@users.noreply.github.com>
2025-10-16 13:39:14 +0200

    last commit

--- src/components/sb/ServerComponent.js
+++ src/components/sb/ServerComponent.js
@@ -1,22 +1,8 @@
-
 import { components } from "@/lib/storyblok"
 import { StoryblokServerComponent } from "@storyblok/react/rsc"     
 import DoesNotExist from "./DoesNotExist"
-
-/*
-Tar emot ett "blok" från Storyblok
-*/
 export default function ServerComponent({ blok }) {
-
-/*
-Hämtar rätt komponentnamn från vår komponentlista (components.js)
-*/
     const Component = components[blok.component];
-
-/*
-Om ingen matchande komponent hittas → rendera fallback-komponenten <DoesNotExist>
-Annars rendera den komponent som matchar blok.component
-*/
     if(!Component) {
         return <DoesNotExist blok={blok} />
     }
