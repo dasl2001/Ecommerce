1076727333d17d5d3e731248aa3e01d59d53ae68
dasl2001 <70586727+dasl2001@users.noreply.github.com>
2025-10-16 13:32:24 +0200

    last commit

--- src/components/sb/DoesNotExist.js
+++ src/components/sb/DoesNotExist.js
@@ -1,5 +1,3 @@
-
-
 export default function DoesNotExist({ blok }) {
     if(process.env.NODE_ENV === 'development') {
     return <div className="bg-red-50 border-red-600 border-dashed border-2 p-4 rounded-md">
