d050f055572f8fbcab732ddf565a8b35acae29f4
dasl2001 <70586727+dasl2001@users.noreply.github.com>
2025-10-16 13:32:11 +0200

    last commit

--- src/components/sb/CallToAction.js
+++ src/components/sb/CallToAction.js
@@ -1,14 +1,9 @@
-
-//color blue, black, red
-
 import { storyblokEditable } from "@storyblok/react";
-
 const colors = {
     blue: "bg-blue-500 text-white hover:bg-blue-600",
     black: "bg-black text-white hover:bg-gray-800",
     red: "bg-red-500 text-white hover:bg-red-600",
 }
-
 export default function CallToAction({ blok }) {
     const colorClass = colors[blok.color]
     return (
