f0948168c58b0f680e1b165001f0af4dc1a2c641
dasl2001 <70586727+dasl2001@users.noreply.github.com>
2025-10-16 13:51:09 +0200

    l

--- src/utils/twMergeUtil.js
+++ src/utils/twMergeUtil.js
@@ -1,7 +1,3 @@
 import { extendTailwindMerge } from 'tailwind-merge';
-
-// Create a custom instance of Tailwind Merge with the desired configuration as a singleton
 const twMerge = extendTailwindMerge({});
-
-// Export the `twMerge` function that merges classes
 export { twMerge };
