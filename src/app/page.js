dff34fb7ebcb2905d5a6cc901bce9247a356f6ce
dasl2001 <70586727+dasl2001@users.noreply.github.com>
2025-10-16 13:30:00 +0200

    last commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import { getStoryblokApi } from "@/lib/storyblok";
 import { StoryblokStory } from '@storyblok/react/rsc';
 import { redirect } from "next/navigation";
-
 export default async function Home() {
   return redirect("/home");
 }
