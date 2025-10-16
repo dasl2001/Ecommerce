8ed6d3251963b852aef42039e22f6705e9230dc5
dasl2001 <70586727+dasl2001@users.noreply.github.com>
2025-10-16 13:48:40 +0200

    last commit

--- src/utils/cn.js
+++ src/utils/cn.js
@@ -1,7 +1,5 @@
 import classNames from 'classnames';
 import { twMerge } from './twMergeUtil';
-
-// Export the `cn` function that merges classes
 export function cn(...classes) {
   return twMerge(classNames(...classes));
 }
